52d80c5369b3b46905d428bb19a6ffd47f18f438
mhauck3 <mhauck3@uic.edu>
2018-03-30 11:43:00 -0500

    Add files via upload

---
+++ app.R
@@ -0,0 +1,91 @@
+# Project 3
+#
+
+library(ggplot2)
+library(data.table)
+library(dplyr)
+library(lubridate)
+library(jpeg)
+library(DT)
+library(grid)
+library(rsconnect)
+library(scales)
+library(pROC)
+library(ROCR)
+library(shiny)
+library(shinydashboard)
+
+# setting the working directory
+setwd("~/Desktop/Tornadoes/Dataset")
+# 
+# data1 = fread("actualTornadoes.csv")
+# nrow(data1)
+data = fread("allTornadoes.csv")
+# nrow(data2)
+names(data)
+newNames = c("tornadoNumber", "year", "month", "day", "date", "time", "timeZone", "state", "fips", "stateNumber", "fscale","injuries", "fatalities", "loss", "cropLoss", "startLat", "startLon", "endLat", "endLon", "length", "width", "numberOfStates", "stateNumber2", "tornadoSegment","fips1st", "fips2nd", "fips3rd", "fips4th","fscale2")
+setnames(data, newNames)
+data
+
+# C1
+tornadoesByYear = data %>%
+  group_by(year, fscale) %>%
+  summarize(tornadoCount = n()) %>%
+  group_by(year) %>%
+  mutate(annualTornadoCount = sum(tornadoCount)) %>%
+  mutate(percTornadoByFscale = tornadoCount/annualTornadoCount) %>%
+  data.table()
+
+tornadoesOverall = nrow(data)
+# nrow(data)
+# ncol(data)
+# dim(data)[1]
+# dim(data)[2]
+
+data %>%
+  group_by(fscale) %>%
+  summarize(totalCount = n())
+
+ggplot(tornadoesByYear, aes(x = factor(year), y = tornadoCount, color = factor(fscale))) + geom_bar(stat = "identity", position = 'dodge')
+
+ggplot(tornadoesByYear, aes(x = factor(year), y = percTornadoByFscale, color = factor(fscale), fill = factor(fscale))) + geom_bar(stat = "identity", position = 'dodge') #sans identity, we get row count -- yikes!
+
+# C2
+tornadoesByMonth = data %>%
+  group_by(month, fscale) %>%
+  summarize(tornadoCount = n()) %>%
+  group_by(month) %>%
+  mutate(monthlyTornadoCount = sum(tornadoCount)) %>%
+  mutate(percTornadoByFscale = tornadoCount/monthlyTornadoCount) %>%
+  data.table()
+
+ggplot(tornadoesByMonth, aes(x = factor(month), y = tornadoCount, color = factor(fscale), fill = factor(fscale))) + geom_bar(stat = "identity", position ='dodge')
+
+# C3 
+tornadoesByHour = data %>%
+  mutate(hour = as.numeric(substr(time,start = 1, stop = 2))) %>%
+  group_by(hour, fscale) %>%
+  summarize(tornadoCount = n()) %>%
+  group_by(hour) %>%
+  mutate(hourlyTornadoCount = sum(tornadoCount)) %>%
+  mutate(percTornadoByFscale = tornadoCount/hourlyTornadoCount) %>%
+  data.table()
+
+ggplot(tornadoesByHour, aes(x = factor(hour), y = tornadoCount, color = factor(fscale), fill = factor(fscale))) + geom_bar(stat = "identity", position ='dodge')
+
+# C4
+# woohoo! It's underway. Add two columns using mutate: startLatLon and endLatLon <-- to calculate distance from Chicago; based on the range, eg. 0-100 km, filter out tornadoes in that range and THEN group it
+
+# C5
+damagesByYear = data %>%
+  group_by(year) %>%
+  summarize(damagesInjuries = sum(injuries),
+            damagesFatalities = sum(fatalities),
+            damagesLoss = sum(loss)) %>%
+  data.table()
+
+ggplot(damagesByYear, aes(x = factor(year), y = damagesInjuries)) + geom_bar(stat = "identity")
+
+ggplot(damagesByYear, aes(x = factor(year), y = damagesFatalities)) + geom_bar(stat = "identity")
+
+#ggplot(damagesByYear, aes(x = factor(year), y = damagesLoss)) + geom_bar(stat = "identity")
